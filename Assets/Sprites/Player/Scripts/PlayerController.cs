3afee70468243f15dadb16420d36516a5b30f61f
YunYun <Janminecraft1234@gmail.com>
2022-01-25 12:12:15 -0500

    Animations and Physics
    Animations and Physics were added, Player Controls

--- Assets/Sprites/Player/Scripts/PlayerController.cs
+++ Assets/Sprites/Player/Scripts/PlayerController.cs
@@ -2,73 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
+
+public class PlayerController : MonoBehaviour    
 {
-    public float playerSpeed = 10;
-    public float jumpForce = 10;
-    // bool to check if player is grounded
-    public bool isGrounded;
-    // variable to cache the player's initial scale, just in case its differeny from 1,1,1
-    private Vector3 initialScale;
-    // Use this for initialization
+
+    public bool verboose = true;
+    public bool isGrounded = false;
+    Animator anim;
+
+    Rigidbody2D rb;
+    SpriteRenderer pr;
+
+    [SerializeField] public float speed;
+
+    [SerializeField] public int jumpForce;
+    [SerializeField] public float groundCheckRadius;
+
+    [SerializeField] LayerMask isGroundLayer;
+    [SerializeField] Transform groundCheck;
+
     void Start()
     {
-        // Cache the scale
-        initialScale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+        pr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        
+        if (speed <= 0) { speed = 5.0f; }
+        if (jumpForce <= 0) { jumpForce = 300; }
+        if (groundCheckRadius <= 0) { groundCheckRadius = 0.05f; }
+        if (!groundCheck) { groundCheck.transform.GetChild(0); }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If right arrow is "held down"
-        if (Input.GetKey(KeyCode.RightArrow))
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
-            // move character to right, deltaTime is the time between frame updates
-            transform.position += playerSpeed * Vector3.right * Time.deltaTime;
-            // flip character
-            transform.localScale = new Vector3(-initialScale.x, initialScale.y, initialScale.z);
-        }
-        // If left arrow is "held down"
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            // move character to left, deltaTime is the time between frame 
-            transform.position += playerSpeed * Vector3.left * Time.deltaTime;
-            // change the scale back to intial so character faces the right direction
-            transform.localScale = new Vector3(initialScale.x, initialScale.y, initialScale.z);
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
         }
 
-        // Commented this, this is one way of checking grounded
-        //isGrounded = (GetComponent<Rigidbody2D>().velocity.y == 0);
+        Vector2 moveDir = new Vector2(hInput * speed, rb.velocity.y);
+        rb.velocity = moveDir;
 
-        // if up arrow is pressed and player is grounded
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
-        {
-            // Add vertical jump force
-            GetComponent<Rigidbody2D>().AddForce(jumpForce * Vector3.up, ForceMode2D.Impulse);
-        }
-    }
+        if (hInput > 0) { pr.flipX = true; }
+        if (hInput < 0) { pr.flipX = false; }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // 8 is ground layer
-        if (collision.gameObject.layer == 8)
-        {
-            isGrounded = true;
-        }
-        // 10 is enemy layer
-        if (collision.collider.gameObject.layer == 10 && !isGrounded)
-        {
-            Destroy(collision.rigidbody.gameObject);
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        // 8 is ground layer
-        if (collision.gameObject.layer == 8)
-        {
-            isGrounded = false;
-        }
+        anim.SetFloat("xVel", Mathf.Abs(hInput));
+        anim.SetBool("isGrounded", isGrounded);
     }
 }
 
